59b7e059aba005ba4284d03f740c890546a058e3
GedionAfework <gedionmekbeb2@gmail.com>
2025-02-28 16:39:55 +0300

    updated-setup

---
+++ Restaurant-Management-System/resources/js/pages/Abcd.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    person: String
+})
+</script>
+
+<template>
+    <div>
+        <h1 class="text-blue-900">
+            My name is {{  person }}
+        </h1>
+    </div>
+</template>
---
+++ Restaurant-Management-System/resources/js/pages/About.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    user: String
+})
+</script>
+
+
+<template>
+    <div>
+        <h1 class="text-red-500">
+        About {{ user }}
+    </h1>
+    </div>
+</template>
